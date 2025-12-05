63c16c6d2776b85b790b45650d33225961a16461
Vinay Singh <109859990+VinaySingh7112@users.noreply.github.com>
2025-12-05 12:46:34 +0530

    Comment out verification submit route
    Comment out the verification submit route for dispatcher addons.

--- routes/web.php
+++ routes/web.php
@@ -1249,7 +1249,7 @@ Route::group(['prefix' => 'owner'], function () {
     //dispatcher addons
 Route::group(['prefix' => 'dispatcher-addons', 'middleware' => 'permission:dispatcher_addons'], function () {
     Route::get('/', [DispatcherAddonsController::class, 'index'])->name('dispatcherAddons.index');
-    Route::post('/verfication-submit', [DispatcherAddonsController::class, 'verification_submit'])->name('dispatcherAddons.verfication-submit');
+   // Route::post('/verfication-submit', [DispatcherAddonsController::class, 'verification_submit'])->name('dispatcherAddons.verfication-submit');
     Route::post('/dipathcer-files', [DispatcherAddonsController::class, 'dispatcher_files_uploads'])->name('dispatcherAddons.dispatcher_files_uploads');
 });
 
